9840a8da9ed5949ff4e5ff36af1bddffaf8d58a1
Sameer Ajmani <sameer@golang.org>
2017-10-27 16:03:29 -0400

    bigger jitter

--- coffee/generate.sh
+++ coffee/generate.sh
@@ -13,6 +13,6 @@ taskset -c 0-5 go run *.go --par=10,100,1000,10000 --dur=4s --mode=ideal,locking
 
 echo jitter
 
-taskset -c 0-5 go run *.go --par=0 --jitter=400us --dur=4s --mode=ideal,locking,finelocking,parsteam,americano,espresso,linearpipe-0,linearpipe-1,linearpipe-10,splitpipe-0,splitpipe-1,americanopipe-0,americanopipe-1,espressopipe-0,espressopipe-1,multi-1,multi-2,multi-4,multipipe-1,multipipe-2,multipipe-4 2> /dev/null > jitter.csv
+taskset -c 0-5 go run *.go --par=0 --jitter=1ms --dur=4s --mode=ideal,locking,finelocking,parsteam,americano,espresso,linearpipe-0,linearpipe-1,linearpipe-10,splitpipe-0,splitpipe-1,americanopipe-0,americanopipe-1,espressopipe-0,espressopipe-1,multi-1,multi-2,multi-4,multipipe-1,multipipe-2,multipipe-4 2> /dev/null > jitter.csv
 
 echo done
