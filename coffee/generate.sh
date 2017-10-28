e5119639ddf412579f32c996870bfd863921824b
Sameer Ajmani <sameer@golang.org>
2017-10-28 08:26:42 -0400

    add multi.csv

--- coffee/generate.sh
+++ coffee/generate.sh
@@ -11,6 +11,10 @@ echo overload
 
 taskset -c 0-5 go run *.go --par=10,100,1000,10000 --dur=4s --mode=ideal,locking,finelocking,parsteam,americano,espresso,linearpipe-0,linearpipe-1,linearpipe-10,splitpipe-0,splitpipe-1,americanopipe-0,americanopipe-1,espressopipe-0,espressopipe-1,multi-1,multi-2,multi-4,multipipe-1,multipipe-2,multipipe-4 2> /dev/null > overload.csv
 
+echo multi
+
+taskset -c 0-5 go run *.go --par=10,100,1000,10000 --dur=4s --mode=multi-1,multi-10,multi-100,multi-1000,multi-10000 2> /dev/null > multi.csv
+
 echo jitter
 
 taskset -c 0-5 go run *.go --par=0 --jitter=1ms --dur=4s --mode=ideal,locking,finelocking,parsteam,americano,espresso,linearpipe-0,linearpipe-1,linearpipe-10,splitpipe-0,splitpipe-1,americanopipe-0,americanopipe-1,espressopipe-0,espressopipe-1,multi-1,multi-2,multi-4,multipipe-1,multipipe-2,multipipe-4 2> /dev/null > jitter.csv
